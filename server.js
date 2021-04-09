e3e4d4545a4c527027a5de85518ff6bc2414caf6
Kayleigh Hanna <kayleighchanna@gmail.com>
2021-04-08 19:13:47 -0500

    added json and dependencies

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
-const routes = require('./controllers');
-
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
@@ -19,12 +21,17 @@ const sess = {
 };
 
 app.use(session(sess));
+const hbs = exphbs.create({helpers});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
+app.use(require('./controllers'));
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+app.listen(PORT, () => {
+  console.log(`Now listening on PORT ${PORT}`)
+  sequelize.sync({force: false})
 });
