8014ed1e6a8b5d96786699deb7b6c37a938812db
Kayleigh Hanna <kayleighchanna@gmail.com>
2021-04-09 10:39:31 -0500

    home-routes framed, api index compelte

---
+++ controllers/home-routes.js
@@ -0,0 +1,43 @@
+const router = require('express').Router();
+const { Homepage, Dashboard, Login } = require('../models');
+const withAuth = require('../utils/auth');
+
+//get Homepage
+router.get('/', async (req, res) => {
+    //get all posts
+    res.render('all');
+});
+
+//get Dashboard
+// Use withAuth middleware to prevent access to route
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Project }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//get Login
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
