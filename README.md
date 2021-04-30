fc0c9ac8294b7589287cb4b2c8201b56066fb936
Kayleigh Hanna <kayleighchanna@gmail.com>
2021-04-29 21:16:52 -0500

    login, logout, singup added, added jass file and style

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Techblog
+
+## User Story
+
+```md
+AS A developer who writes about tech
+I WANT a CMS-style blog site
+SO THAT I can publish articles, blog posts, and my thoughts and opinions
+```
+
+## Tech Stack
+
+<li>Model View Controller(MVC)</li>
+<li>Handlebars</li>
+<li>Sequelize</li>
+<li>Heroku</li>
+<li>MySQL</li>
--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -13,6 +13,26 @@ router.post('/', withAuth, async (req, res) => {
     }
 })
 //put route catch is same
+router.put('/', withAuth, async (req, res) => {
+    //update post
+    const body = req.body;
+    try {
+        const post = await Post.update({...body, userId: req.session.userId})
+        res.json(post);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 //delete route cathc is same
+router.delete('/', withAuth, async (req, res) => {
+   //delete post
+    const body = req.body;
+    try {
+        const newPost = await Post.delete({...body, userId: req.session.userId})
+        res.json(newPost);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
 module.exports = router
